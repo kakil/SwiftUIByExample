a6c54fc4513cbb861be0b27618d940128b68762b
Kitwana Akil <kakil@me.com>
2019-06-09 16:45:50 -0400

    Initial Commit

---
+++ SwiftUIByExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIByExample
+//
+//  Created by Kitwana Akil on 6/9/19.
+//  Copyright © 2019 Kitwana Akil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
