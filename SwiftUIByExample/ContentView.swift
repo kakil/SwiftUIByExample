bcbdba2f5366984c260721b6a355921a481fd490
Kitwana Akil <kakil@me.com>
2019-06-09 17:12:25 -0400

    Completed lesson about customizing stack layouts with alignment and spacing.

--- SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading, spacing: 20) {
+            Text("SwiftUI")
+            Text("Rocks")
+        }
     }
 }
 
